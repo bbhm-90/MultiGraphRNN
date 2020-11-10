37f54bdcf739b1f0c82a2a8b661c709ff10414fb
Bahador Bahmani <bahador@dyn-160-39-162-243.dyn.columbia.edu>
2020-11-10 14:44:20 -0500

    deleted not used files

--- scripts/bulk_plasticity_classical_with_fabric/__run.py
+++
@@ -1,6 +0,0 @@
-import os
-start_id = [5,17,29,41,53]
-end_id = [8,21,32,44,57]
-assert len(start_id) == len(end_id)
-for i,_ in enumerate(start_id):
-    os.system("python3 scripts/bulk_plasticity_classical_without_fabric/predict_mean.py {} {}".format(start_id[i], end_id[i]))
